6aa395bcfb2e0af307c4487d718c250710c155d6
mehranulhassan <l1f19bsse0239@ucp.edu.pk>
2023-08-30 20:44:31 +0500

    second push

---
+++ Models/todosmodel.js
@@ -0,0 +1,12 @@
+class Todos{
+    constructor(name,id ,age,completed)
+    {
+        this.name=name;
+        this.id=id;
+        this.age=age;
+        this.completed=completed;
+    }
+   
+
+}
+module.exports= Todos;
